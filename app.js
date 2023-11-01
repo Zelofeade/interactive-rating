af9eed69da60d1b0c26f64cd8760a52f2181783d
Artur <artur.cavalcante3107@gmail.com>
2023-11-01 09:21:30 -0300

    phone size finished

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const ratings = document.querySelectorAll(".ratings");
+const button = document.querySelector("button");
+const container = document.querySelector(".second");
 
 ratings.forEach((rating_card) => {
 	rating_card.addEventListener("click", (e) => {
+		const selected = document.querySelector(".selected > span");
 		const active = document.querySelector(".checked");
 		if (active) {
 			active.classList.remove("checked");
 		}
 		const card = e.target;
 		card.classList.add("checked");
+
+		selected.textContent = card.textContent;
 	});
+});
+
+button.addEventListener("click", (e) => {
+	const active = document.querySelector(".checked");
+	if (active) {
+		container.classList.remove("hidden");
+	}
 });
